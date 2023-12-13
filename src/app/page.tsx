61b2de5c369af3240def64683d3b30d982b8a9c9
adriano hardtke <adr.hardtke@gmail.com>
2023-12-13 18:09:47 -0300

    remove desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,22 +14,38 @@ import { Footer } from "@/sessions/footer/Footer";
 export default function Home() {
   return (
     <>
-      <Hero />
-      <div className="p-4 sm:max-w-4xl sm:mx-auto">
-        <Sobre />
-        <Ajuda />
-        <Avaliacoes />
+    <div className="md:block hidden">
+      <div className='w-screen h-screen flex items-center justify-center flex-col gap-8'>
+            <p className='text-center text-3xl'>A página web está em construção, por favor, acesse pelo seu smartphone.</p>
+            <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+              >Loading...</span
+            >
+          </div>
+        </div>
+
+    </div>
+      <div className="md:hidden block">
+        <Hero />
+        <div className="p-4 sm:max-w-4xl sm:mx-auto">
+          <Sobre />
+          <Ajuda />
+          <Avaliacoes />
+        </div>
+        <FirstCTA />
+        <div className="p-4 sm:max-w-4xl sm:mx-auto">
+          <Contato />
+          <ComoFunciona />
+          <NossoEspaco />
+          <Parcerias />
+        </div>
+        <SecondCTA />
+        <InstagramCTA />
+        <Footer />
       </div>
-      <FirstCTA />
-      <div className="p-4 sm:max-w-4xl sm:mx-auto">
-        <Contato />
-        <ComoFunciona />
-        <NossoEspaco />
-        <Parcerias />
-      </div>
-      <SecondCTA />
-      <InstagramCTA />
-      <Footer />
     </>
   )
 }
