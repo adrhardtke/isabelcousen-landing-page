d6be6f49da7085285c15e046eb9fb1d1f11fa310
adriano hardtke <adr.hardtke@gmail.com>
2023-12-14 12:25:19 -0300

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Nutricionista Isabel Cousen',
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={inter.className}>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
