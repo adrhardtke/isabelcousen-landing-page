44b0851b10d2edf0c852fb4955ea1c0c86fe5c9b
adriano hardtke <adr.hardtke@gmail.com>
2023-12-14 15:32:12 -0300

    remove parcerias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
           <Contato />
           <ComoFunciona />
           <NossoEspaco />
-          <Parcerias />
+          {/* <Parcerias /> */}
         </div>
         <SecondCTA />
         <InstagramCTA />
